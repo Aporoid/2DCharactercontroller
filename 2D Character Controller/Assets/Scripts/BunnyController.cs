40ad947be31fc5f36ed56a0c1b930e974dc267c7
Kyle Cook <Kyle.Cook@iam.local>
2018-10-17 14:18:26 -0500

    Running
    ... around at the speed of sound

--- 2D Character Controller/Assets/Scripts/BunnyController.cs
+++ 2D Character Controller/Assets/Scripts/BunnyController.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class BunnyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private new Rigidbody2D rigidbody2D;
+
+    public float maxSpeed = 10f;
+
+    private bool facingRight = true;
+
+    Animator anim;
+    
+	void Start ()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move <0 && facingRight)
+        {
+            Flip();
+        }
 	}
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
